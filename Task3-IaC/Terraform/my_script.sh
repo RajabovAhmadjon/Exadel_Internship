63a3c2f0b9beeef1bc9ce7033402eac1a27335b6
Akhmadjon Rajabov <r_ahmadjon@mail.ru>
2022-02-28 20:19:28 +0500

    Last changes

--- Task3-IaC/Terraform/my_script.sh
+++ Task3-IaC/Terraform/my_script.sh
@@ -1,21 +1,21 @@
 # !/bin/bash
-sudo apt update
-sudo apt install apache2
-sudo systemctl enable apache2.service
-sudo systemctl start apache2.service
+apt update
+apt install apache2
+systemctl enable apache2.service
+systemctl start apache2.service
 
-sudo ufw allow 80/tcp comment 'accept Apache'
-sudo ufw allow 443/tcp comment 'accept HTTPS connections'
+ufw allow 80/tcp comment 'accept Apache'
+ufw allow 443/tcp comment 'accept HTTPS connections'
 
 MYOS1=`grep '^VERSION' /etc/os-release | head -1`
 MYOS2=`egrep '^(VERSION|NAME)=' /etc/os-release | head -1`
 echo "<h2> Hello World !!! </h2><p>OS Info: $MYOS1 - $MYOS2</p><br> Built by Terraform" > index.html
-sudo mv index.html /var/www/html
+mv index.html /var/www/html
 
-sudo apt install docker.io
-sudo snap install docker
+apt install docker.io
+snap install docker
 docker --version
-sudo docker run hello-world
+docker run hello-world
 LIST=`sudo docker images`
 echo "<form>$LIST</form>">>index.html
-sudo mv index.html /var/www/html
+mv index.html /var/www/html
--- Task3-IaC/Terraform/provider.tf
+++ Task3-IaC/Terraform/provider.tf
@@ -1,3 +1,3 @@
 provider "aws" {
-  region = "us-west-1" # this will make default region as ap-south-1 which is in mumbai
+  region = "us-east-1" # this will make default region as ap-south-1 which is in mumbai
 }
